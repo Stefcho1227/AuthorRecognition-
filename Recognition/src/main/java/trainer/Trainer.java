159ef3ef9022937a879e6a29ded4fd53ed0ed4f2
Ani Bozhkova <ani2003ab@gmail.com>
2025-01-13 16:27:42 +0200

    added some resource files for Vazov

---
+++ Recognition/src/main/java/trainer/Trainer.java
@@ -0,0 +1,14 @@
+package trainer;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.util.*;
+
+public class Trainer {
+    public static void main(String[] args) {
+        // Start implementing your training logic here.
+        System.out.println("Hello, Trainer!");
+    }
+}
